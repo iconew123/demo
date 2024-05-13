bab860637d0f5d5d7599cf44c7110e4cb2cf37f4
iconew123 <iconew123@gmail.com>
2024-05-13 18:15:00 +0900

    Create Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// react component APP 이라고 정의
+// JSX >> Javascript Xml 이라고 함
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello World!</h1>
   );
 }
 
